b62ead970ec4ea1fa9360c6f97da0ddec2846d1c
cdk <everydance075@gmail.com>
2026-03-30 18:45:53 +0800

    Create signalexample.lua

---
+++ signalexample.lua
@@ -0,0 +1,21 @@
+	local Signal = require(signal)
+	local sig = Signal.new()
+
+	local conn = sig:Connect(function(...)
+		print(...)
+	end)
+
+	sig:Fire("hi")
+	conn:Disconnect()
+
+	sig:Once(function(msg)
+		print(msg)
+	end)
+
+	task.spawn(function()
+		local msg = sig:Wait()
+		print(msg)
+	end)
+
+	sig:DisconnectAll()
+	sig:Destroy()
